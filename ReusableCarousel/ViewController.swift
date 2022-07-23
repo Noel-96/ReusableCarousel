bae253e534d3a25432bd278a4811ac6371212678
Noel-96 <noelorion69@gmail.com>
2022-07-23 08:30:37 +0100

    added defualt files

---
+++ ReusableCarousel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReusableCarousel
+//
+//  Created by Noel Obaseki on 23/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
